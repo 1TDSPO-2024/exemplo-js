532fc22fb78122cbf06dc96a5658bfc99932e746
SunaUezuri <wesley.jane.santos@gmail.com>
2024-05-09 22:19:05 -0300

    Logout e memória

---
+++ js/script-sucesso.js
@@ -0,0 +1,16 @@
+
+
+
+//Recuperando o Objeto do localStorage
+let usuario = JSON.parse(localStorage.getItem("usuario-validado"));
+
+//Escrevendo na mensagem de Boas Vindas o nome do Usuário:
+const msgWelcome = document.querySelector("#msg-welcome");
+msgWelcome.innerText = usuario.nomeCompleto;
+
+//Criando um evento na tag a para realizar o logout do Usuário:
+const userLogout = documentc.querySelector("#logout-user");
+userLogout.addEventListener("click", ()=>{
+    localStorage.removeItem("usuario-validado");
+    window.location.href = "../index.html";
+});

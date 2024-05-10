f953fadda2a2b1b7434626357432697f5213bb70
SunaUezuri <wesley.jane.santos@gmail.com>
2024-05-09 22:45:51 -0300

    Form de cadastro criado

--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -4,6 +4,7 @@
 //Recuperando o Objeto do localStorage
 let usuario = JSON.parse(localStorage.getItem("usuario-validado"));
 
+if (usuario){
 //Escrevendo na mensagem de Boas Vindas o nome do Usuário:
 const msgWelcome = document.querySelector("#msg-welcome");
 msgWelcome.innerText = usuario.nomeCompleto;
@@ -14,3 +15,6 @@ userLogout.addEventListener("click", ()=>{
     localStorage.removeItem("usuario-validado");
     window.location.href = "../index.html";
 });
+}else{
+    window.location.href = "../status/erro.html";
+}
